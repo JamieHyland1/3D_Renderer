9b4fcf9544708c213554f6f5cca93125f27e4d7e
Jamie <jamiehyland01@gmail.com>
2023-09-09 19:43:52 +0100

    added vector operations along with backface culling

--- 3DRenderer/src/display.c
+++ 3DRenderer/src/display.c
@@ -53,7 +53,7 @@ bool initialize_window(void){
         return false;
     }
 
-    SDL_SetWindowFullscreen(window,SDL_WINDOW_BORDERLESS);
+    SDL_SetWindowBordered(window,SDL_WINDOWPOS_CENTERED);
 
     return true;
 }
@@ -136,30 +136,30 @@ void render_color_buffer(void){
     SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
 }
 
-void draw_line(int x1, int y1, int x2, int y2, uint32_t color){
-    int deltaX = x2-x1;
-    int deltaY = y2-y1;
+void draw_line(int x0, int y0, int x1, int y1, uint32_t color) {
+    int delta_x = (x1 - x0);
+    int delta_y = (y1 - y0);
 
-    int side_length = abs(deltaX) >= abs(deltaY) ? abs(deltaX) : abs(deltaY);
+    int longest_side_length = (abs(delta_x) >= abs(delta_y)) ? abs(delta_x) : abs(delta_y);
 
-    float x_inc = deltaX / (float)side_length;
-    float y_inc = deltaY / (float)side_length;
+    float x_inc = delta_x / (float)longest_side_length; 
+    float y_inc = delta_y / (float)longest_side_length;
 
-    float currentX = x1;
-    float currentY = y1;
-
-    for(int i = 0; i < side_length; i ++){
-        drawPixel(round(currentX),round(currentY), color);
-        currentX += x_inc;
-        currentY += y_inc;
+    float current_x = x0;
+    float current_y = y0;
+    for (int i = 0; i <= longest_side_length; i++) {
+        drawPixel(round(current_x), round(current_y), color);
+        current_x += x_inc;
+        current_y += y_inc;
     }
 }
 
-void draw_triangle(int x1, int y1, int x2, int y2, int x3, int y3, uint32_t color){
+void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+    draw_line(x0, y0, x1, y1, color);
     draw_line(x1, y1, x2, y2, color);
-    draw_line(x2, y2, x3, y3, color);
-    draw_line(x3, y3, x1, y1, color);
+    draw_line(x2, y2, x0, y0, color);
 }
+
 
 void destroy_window(void){
    
--- 3DRenderer/src/vector.c
+++ 3DRenderer/src/vector.c
@@ -1,5 +1,116 @@
 #include <math.h>
 #include "vector.h"
+////////////////////////////////////////
+/// Vector 2D functions Implementations
+////////////////////////////////////////
+float vec2_length(vec2_t v){
+    return sqrt((v.x * v.x) + (v.y * v.y));
+}
+
+vec2_t vec2_add(vec2_t a, vec2_t b){
+    vec2_t x = {
+        .x = a.x + b.x,
+        .y = a.y + b.y
+    };
+    return x;
+}
+
+vec2_t vec2_sub(vec2_t a, vec2_t b){
+    vec2_t x = {
+        .x = a.x - b.x,
+        .y = a.y - b.y
+    };
+    return x;
+}
+
+vec2_t vec2_mult(vec2_t a, float f){
+    vec2_t x = {
+        .x = a.x * f,
+        .y = a.y * f
+    };
+    return x;
+}
+
+vec2_t vec2_div(vec2_t a, float f){
+    vec2_t x = {
+        .x = a.x / f,
+        .y = a.y / f
+    };
+    return x;
+}
+void vec2_normalize(vec2_t* v){
+    float l = vec2_length(*v);
+    v->x /= l;
+    v->y /= l;
+}
+
+float vec2_dot(vec2_t a, vec2_t b){
+    return (a.x * b.x) + (a.y * b.y);
+}
+
+////////////////////////////////////////
+/// Vector 2D functions Implementations
+////////////////////////////////////////
+float vec3_length(vec3_t v){
+    return sqrt((v.x * v.x) + (v.y * v.y) + (v.z * v.z));
+}
+
+vec3_t vec3_add(vec3_t a, vec3_t b){
+    vec3_t x = {
+        .x = a.x + b.x,
+        .y = a.y + b.y,
+        .z = a.z + b.z
+    };
+    return x;
+}
+
+vec3_t vec3_sub(vec3_t a, vec3_t b){
+    vec3_t x = {
+        .x = a.x - b.x,
+        .y = a.y - b.y, 
+        .z = a.z - b.z
+    };
+    return x;
+}
+
+vec3_t vec3_mult(vec3_t a, float f){
+    vec3_t x = {
+        .x = a.x * f,
+        .y = a.y * f,
+        .z = a.z * f
+    };
+    return x;
+}
+
+vec3_t vec3_div(vec3_t a, float f){
+    vec3_t x = {
+        .x = a.x / f,
+        .y = a.y / f,
+        .z = a.z / f
+    };
+    return x;
+}
+
+float vec3_dot(vec3_t a, vec3_t b){
+    return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+}
+
+vec3_t vec3_cross(vec3_t a, vec3_t b){
+    vec3_t result = {
+        .x = a.y * b.z - a.z * b.y,
+        .y = a.z * b.x - a.x * b.z,
+        .z = a.x * b.y - a.y * b.x
+    };
+
+    return result;
+}
+
+void vec3_normalize(vec3_t* v){
+    float l = vec3_length(*v);
+    v->x /= l;
+    v->y /= l;
+    v->z /= l;
+}
 
  vec3_t vec3_rotate_x(vec3_t v, float angle){
     vec3_t rotate_vector = {
