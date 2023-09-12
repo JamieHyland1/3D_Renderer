3da6a80a1e08d2cd7187689bc267d58ff9e079bd
Jamie <jamiehyland01@gmail.com>
2023-09-12 17:42:38 +0100

    added rudimentary depth sorting to triangles

--- 3DRenderer/src/triangle.h
+++ 3DRenderer/src/triangle.h
@@ -10,8 +10,8 @@ typedef struct {
 
 typedef struct {
     vec2_t points[3];
+    float avg_depth;
 }triangle_t;
-
 
 void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2,int y2, uint32_t color);
 
