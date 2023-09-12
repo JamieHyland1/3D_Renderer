3d5ae0060c459d02ef2a51b1cce1f3eee1954cca
Jamie <jamiehyland01@gmail.com>
2023-09-12 16:45:50 +0100

    added triangle rasterization

--- 3DRenderer/src/triangle.c
+++ 3DRenderer/src/triangle.c
@@ -58,13 +58,17 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2,int y2, uint32_
         int_swap(&y0,&y1);
         int_swap(&x0,&x1);
     }
+    if(y1 == y2){
+        //triangle is already a flat bottom triangle
+        fill_flat_bottom_triangle(x0,y0,x1,y1,x2,y2,color);
+    
+    }else if(y0 == y1){
+        fill_flat_top_triangle(x0,y0,x1,y1,x2,y2,color);
+    }else{
+        int My = y1;
+        int Mx = ( ( (float)(x2-x0)*(y1-y0) ) /(float) (y2-y0) ) + x0;
 
-
-    int My = y1;
-    int Mx = ( ( (float)(x2-x0)*(y1-y0) ) /(float) (y2-y0) ) + x0;
-
-    fill_flat_bottom_triangle(x0,y0,x1,y1,Mx,My,0xFFFFFF);
-    fill_flat_top_triangle(x1,y1,Mx,My,x2,y2,0xFFFFFF);
-
-
+        fill_flat_bottom_triangle(x0,y0,x1,y1,Mx,My,color);
+        fill_flat_top_triangle(x1,y1,Mx,My,x2,y2,color);
+    }
 }
